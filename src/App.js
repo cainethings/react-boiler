ea2166cf368c6dc517dc60196d6e8ed5d2af7600
Caine Daniel <admin@cainethings.com>
2023-03-06 09:08:05 +0530

    Adding the basic react boiler setup:
    This setup is inspired from https://www.geeksforgeeks.org/building-a-react-boilerplate-from-scratch-without-using-create-react-app/

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.scss";
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">React Boilerplate</h1>
+      <h4 className="sub-heading">by CaineThings</h4>
+    </div>
+  );
+};
+
+export default App;
